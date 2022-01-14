0474a4c6c75b139fba80a82bf93410853ae72578
Frus <mcagdasbayirli@gmail.com>
2022-01-14 19:28:43 +0300

    init

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,16 +5,14 @@ const Navbar = () => {
   return (
     <nav className="absolute inset-x-0 top-0 z-10">
       <div className="flex justify-between items-center lg:px-[70px] sm:px-12 px-2 py-5">
-        <a href="/">
-          <Image
-            src={Logo}
-            alt="the5starz Brand Logo"
-            className="object-cover"
-            placeholder="blur"
-            width={212}
-            height={78}
-          />
-        </a>
+        <Image
+          src={Logo}
+          alt="the5starz Brand Logo"
+          className="object-cover"
+          placeholder="blur"
+          width={212}
+          height={78}
+        />
         <div>
           <button className="bg-primaryRed text-white lg:py-3 lg:px-5 sm: py-2 px-3 uppercase text-sm font-semibold">
             Let's Talk
