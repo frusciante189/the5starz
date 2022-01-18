8fb15a3cd02e33fe86bed5facfd8f621a5a1f6db
Frus <mcagdasbayirli@gmail.com>
2022-01-18 23:01:41 +0300

    close icon

--- components/Home/Inspired.js
+++ components/Home/Inspired.js
@@ -3,24 +3,18 @@ import React, { useState } from "react";
 
 const Inspired = () => {
   const [image, setImage] = useState("");
-  const [video, setVideo] = useState("");
   const [open, setOpen] = useState(false);
-  const [videoPath, setVideoPath] = useState(null);
-
-  const clickHandle = () => {
-    setOpen(true);
-  };
 
   return (
     <section className="bg-[#D6D4D5]">
       <div className="lg:px-[70px] sm:px-12 p-5 sm:py-7 md:py-10 lg:py-12 xl:py-16">
         <h1 className="text-center text-[28px] leading-[28px]">Get inspired</h1>
       </div>
-      <div className="mt-10 max-w-7xl mx-auto lg:columns-4 columns-2 break-before-avoid gap-12">
+      <div className="mt-10 max-w-7xl mx-auto lg:columns-4 columns-2 break-before-avoid gap-6">
         <img
           src="https://the5starz.com/wp-content/uploads/2021/11/pro8.jpg"
           alt=""
-          onClick={clickHandle}
+          onClick={() => setOpen(true)}
         />
         <img
           src="https://the5starz.com/wp-content/uploads/2021/12/0a7bae_f228cfc6624042f69687c777945a9596_mv2.jpeg"
--- components/Home/Lightbox.js
+++ components/Home/Lightbox.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactPlayer from "react-player";
+import { XCircleIcon } from "@heroicons/react/solid";
 
 const Lightbox = ({ src, open, setOpen, vid }) => {
   return (
@@ -12,12 +13,18 @@ const Lightbox = ({ src, open, setOpen, vid }) => {
     >
       <div className="flex items-center justify-center h-full">
         <ReactPlayer
-          url="./Assets/gallery/v1.mp4"
+          url="https://the5starz.com/wp-content/uploads/2021/12/video-output-65990724-965D-4E90-A801-4251537FB303-1.mov"
           width="50%"
           height="50%"
           controls={true}
         />
       </div>
+      <span
+        className="fixed right-0 top-0 text-white p-5 cursor-pointer"
+        onClick={() => setOpen(false)}
+      >
+        <XCircleIcon className="w-10 h-10" />
+      </span>
     </div>
   );
 };
