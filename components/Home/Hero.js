3a3549a7ab08324f89cbc3d067ece2bd46d19d59
Frus <mcagdasbayirli@gmail.com>
2022-01-22 21:57:18 +0300

    scroll and routes

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { BsArrowRight } from "react-icons/bs";
+import Link from "next/link";
 
 const Hero = () => {
+  const handleScroll = () => {
+    window.scroll({
+      top: document.body.offsetHeight,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className="bg-banner bg-no-repeat lg:h-[830px] h-[550px] md:bg-center bg-cover bg-custom">
       <div className="lg:px-[70px] sm:px-12 px-5 h-full pb-10 md:pb-0">
@@ -17,18 +26,23 @@ const Hero = () => {
             people in return for their authentic, homemade content.
           </p>
           <div className="flex md:flex-row flex-col md:space-x-4 space-y-4 md:space-y-0 md:mt-8 mt-5 w-3/4 md:w-2/3 lg:w-1/3">
-            <button className="bg-primaryRed px-5 py-4 text-white font-bold uppercase flex items-center justify-between w-full">
+            <button
+              className="bg-primaryRed px-5 py-4 text-white font-bold uppercase flex items-center justify-between w-full"
+              onClick={handleScroll}
+            >
               Book a Demo
               <span>
                 <BsArrowRight size={18} className="inline-block ml-2" />
               </span>
             </button>
-            <button className="bg-primaryRed px-5 py-4 text-white font-bold uppercase flex items-center justify-between w-full">
-              Sign Up for free
-              <span>
-                <BsArrowRight size={18} className="inline-block ml-2" />
-              </span>
-            </button>
+            <Link href="join-our-community">
+              <button className="bg-primaryRed px-5 py-4 text-white font-bold uppercase flex items-center justify-between w-full">
+                Sign Up for free
+                <span>
+                  <BsArrowRight size={18} className="inline-block ml-2" />
+                </span>
+              </button>
+            </Link>
           </div>
         </div>
       </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,34 @@
 import Image from "next/image";
+import Link from "next/link";
 import Logo from "../public/Assets/logo.png";
 
 const Navbar = () => {
+  const handleScroll = () => {
+    window.scroll({
+      top: document.body.offsetHeight,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <nav className="absolute inset-x-0 top-0 z-10">
       <div className="flex justify-between items-center lg:px-[70px] sm:px-12 px-2 py-5">
-        <Image
-          src={Logo}
-          alt="the5starz Brand Logo"
-          className="object-cover"
-          placeholder="blur"
-          width={212}
-          height={78}
-        />
+        <Link href="/">
+          <Image
+            src={Logo}
+            alt="the5starz Brand Logo"
+            className="object-cover"
+            placeholder="blur"
+            width={212}
+            height={78}
+          />
+        </Link>
         <div>
-          <button className="bg-primaryRed text-white lg:py-3 lg:px-5 sm: py-2 px-3 uppercase text-sm font-semibold">
+          <button
+            className="bg-primaryRed text-white lg:py-3 lg:px-5 sm:py-2 px-3 uppercase text-sm font-semibold"
+            onClick={handleScroll}
+          >
             Lets Talk
           </button>
         </div>
